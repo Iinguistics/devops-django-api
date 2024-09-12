a86a94016447461863255ec36d7c77982a6e79cb
James <Thylinguistics@gmail.com>
2024-09-12 13:28:52 -0700

    adds rds resources

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -12,3 +12,12 @@ variable "contact" {
   description = "Contact name for tagging resources"
   default     = "jmsgoytia@gmail.com"
 }
+
+variable "db_username" {
+  description = "Username for the dda api database"
+  default     = "dda"
+}
+
+variable "db_password" {
+  description = "Password for the Terraform database"
+}
