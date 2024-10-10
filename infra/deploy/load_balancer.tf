0be616c863810c59faca2ab083192079818e22fd
James <Thylinguistics@gmail.com>
2024-10-10 16:10:43 -0700

    adds load balancer target group

--- infra/deploy/load_balancer.tf
+++ infra/deploy/load_balancer.tf
@@ -38,3 +38,18 @@ resource "aws_lb" "api" {
   subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
   security_groups    = [aws_security_group.lb.id]
 }
+
+# ecs will have an auto generated internal ip 
+# target_type: interal ip address to the running task in ecs
+# lb will accept req on port 443 https then forward to port 8000 (proxy) -> api
+resource "aws_lb_target_group" "api" {
+  name        = "${local.prefix}-api"
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+  port        = 8000
+
+  health_check {
+    path = "/api/health-check/"
+  }
+}
