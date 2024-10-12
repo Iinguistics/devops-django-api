314468c7f60f0b7c3e7f03c6a7585cf6ec93f922
James <Thylinguistics@gmail.com>
2024-10-12 13:43:33 -0700

    adds efs sec groups

--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -8,3 +8,20 @@ resource "aws_efs_file_system" "media" {
     Name = "${local.prefix}-media"
   }
 }
+
+resource "aws_security_group" "efs" {
+  name   = "${local.prefix}-efs"
+  vpc_id = aws_vpc.main.id
+
+# 2049 default Network file system port
+  ingress {
+    from_port = 2049
+    to_port   = 2049
+    protocol  = "tcp"
+
+# only allow ecs access
+    security_groups = [
+      aws_security_group.ecs_service.id
+    ]
+  }
+}
